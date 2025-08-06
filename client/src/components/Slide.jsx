af7b2cbec8abb3494204c60c1cafa0553037e1fc
Rahima Akter <139994153+CODERRAHIMA@users.noreply.github.com>
2025-08-06 20:14:41 +0600

    Add files via upload
    adding client folder

---
+++ client/src/components/Slide.jsx
@@ -0,0 +1,13 @@
+import "../styles/Slide.scss";
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>Welcome Home!!</h1>
+      <h2>Anywhere You Roam, Stay In The Moment & Make Unforgettable Memories</h2>
+      <h5> - Let's embark on your dream journey </h5>
+    </div>
+  );
+};
+
+export default Slide;
